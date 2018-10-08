37fc8a1a865d09a6bb16fbde4fb9521b94b634ff
masi-sh <37276493+masi-sh@users.noreply.github.com>
2018-10-08 00:40:50 -0400

    Update parallel.py

--- parallel.py
+++ parallel.py
@@ -7,12 +7,16 @@ import plotly
 import plotly.offline as py
 import plotly.graph_objs as go
 cf.go_offline()
-df = pd.read_csv("~/DP_out_Sum/dataset/combined.csv")
+df = pd.read_csv("~/Downloads/SalaryData/combined.csv")
 import matplotlib.pyplot as plt
 from itertools import combinations
 from sklearn.neighbors import LocalOutlierFactor
 from collections import Counter
 import time
+import fcntl
+
+outputname  = '~/Downloads/SalaryData/Outputs/output'+sys.argv[1]+'.txt'
+Maxfilename = '~/Downloads/SalaryData/Max.txt'
 
 
 emp_counts = df['Employer'].value_counts()
@@ -48,8 +52,13 @@ ThrAtt_Sprset = sum(map(lambda r: list(combinations(ThrAtt_lst[5:], r)), range(1
 Sub_pop        =  []
 Sub_pop_count  =  0
 Epsilon        =  0.1  ### Privacy Parameter
+output         =  []
+context        =  []
 
 t0 = time.time()
+outputfile = open(outputname,'w')
+Maxfile    = open(Maxfilename, 'a')
+
 ############################## Finds Maximal ##########################################
 for i in range (0, len(FirAtt_Sprset)):
  for j in range(int(sys.argv[1]), (int(sys.argv[1])+1)):
@@ -63,31 +72,56 @@ for i in range (0, len(FirAtt_Sprset)):
             print '\n\n The ', j, 'th element in the Second attribute\'s superset'
             print '\n\n The ', z, 'th element in the third attribute\'s superset'
             for row in range(df2.shape[0]):
-               # FirAtt referes to 'Job Title', which is array cell #5, 
-               # SecAtt referes to 'Employer', which is array cell #4,
-               # ThrAtt referes to 'Calendar Year', which is array cell #7,
-               # isnt union1d(FirAtt_Sprset[i], FirAtt_lst[:5])= FirAtt_Sprset[i]+ FirAtt_lst[:5]
+		# print '\n\nrow is' , df2.iloc[row]
+                # FirAtt referes to 'Job Title', which is array cell #5, 
+                # SecAtt referes to 'Employer', which is array cell #4,
+                # ThrAtt referes to 'Calendar Year', which is array cell #7,
+                # isnt union1d(FirAtt_Sprset[i], FirAtt_lst[:5])= FirAtt_Sprset[i]+ FirAtt_lst[:5]
                 if ((df2.iloc[row][5] in (np.union1d(FirAtt_Sprset[i], FirAtt_lst[:5]))) & \
                     (df2.iloc[row][4] in  (np.union1d(SecAtt_Sprset[j], SecAtt_lst[:5]))) & \
                      (df2.iloc[row][7] in (np.union1d(ThrAtt_Sprset[z], ThrAtt_lst[:5])))):
                     pop_size += 1
                     Sal_list.append(df2.iloc[row]['Salary Paid'])
-                    ID_list.append(df2.iloc[row]['Unnamed: 0'])
+		    ID_list.append(df2.iloc[row]['Unnamed: 0'])
+
+
+##########################3                                     ##########################
+
+
             print '\n\n Population size for orignial context plus first attribute set=', i, \
                     'Second attribute set=', j, 'Third attribute set=', z , 'is' , pop_size
 #####################         Outlier detection in subpopulations      ########################
-            if (pop_size >= 4):
-                Score = np.exp(Epsilon *(pop_size** (1. / 3)))
+            if (pop_size >= 20):
+                #Score = np.exp(Epsilon *(pop_size** (1. / 3)))
                 Sal_arr= np.array(Sal_list)
-                clf = LocalOutlierFactor(n_neighbors=4)
+                clf = LocalOutlierFactor(n_neighbors=20)
                 Sal_outliers = clf.fit_predict(Sal_arr.reshape(-1,1))
+		context.append([i,j,z,pop_size])
+		outputfile.write('Context:\n'+str(context)+'\n')
+		print '\n\nlen(ID_list) is', len(ID_list)
+		print '\n\nSal_outliers for the context++ is', Sal_outliers, '\n\n An example of outlier here is', df2.iloc[Sal_outliers.argmin()][1]
                 for outlier_finder in range(0, len(ID_list)):
-                    if ((ID_list[outlier_finder]==Queried_ID) & (Sal_outliers[outlier_finder]==-1)): 
-                        Sub_pop.append([i,j,z,pop_size, Score, Sub_pop_count])
-                        Sub_pop_count += 1
-                    print '\n\nSubpopulations are[Att1_index, Att2_index, Att3_index, Population_size, Score, ID]\n\n', Sub_pop	
- 
+                    if (Sal_outliers[outlier_finder]==-1): 
+			output.append(ID_list[outlier_finder]) 
+			if (ID_list[outlier_finder]==Queried_ID):                       
+				Sub_pop.append([i,j,z,pop_size, Score, Sub_pop_count])
+                        	Sub_pop_count += 1
+		Sub_pop_sorted = sorted(Sub_pop,key=lambda Sub_pop: Sub_pop[3])
+		
+		print '\n\nSubpopulations are[Att1_index, Att2_index, Population_size, Score, ID]\n\n', Sub_pop	
+		print '\n\nSubpopulations sorted based on the population size are[Att1_index, Att2_index, Att2_index, Population_size, Score, ID]\n\n', \
+                Sub_pop_sorted
+		print '\n\n str(output)', str(output)
+		outputfile.write('ID_list:\n'+str(output)+'\n')
+
+outputfile.close()
+if Sub_pop_sorted:
+	fcntl.flock(Maxfile, fcntl.LOCK_EX)
+	Maxfile.write(str(Sub_pop_sorted[Sub_pop_count-1])+'\n')
+  fcntl.flock(Maxfile, fcntl.LOCK_UN)	
+	print 'Max_population is', Sub_pop_sorted[Sub_pop_count-1]   
+
+Maxfile.close()
+
 t1 = time.time()
 print '\n\nThe required time for running the program is:',  t1-t0
-
-
