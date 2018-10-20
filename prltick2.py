aded185e3866789cba9b1a2f690a7ecd513a0b0d
masi-sh <37276493+masi-sh@users.noreply.github.com>
2018-10-20 07:29:17 -0400

    Update prltick2.py

--- prltick2.py
+++ prltick2.py
@@ -61,7 +61,7 @@ Maxfile    = open(Maxfilename, 'a')
 
 ############################## Finds Maximal ##########################################
 for i in range (int(sys.argv[1]), (int(sys.argv[1])+1)):
- for j in range(0,32):
+ for j in range(32,64):
    for z in range(0, len(ThrAtt_Sprset)):
             Sal_list   = []
             ID_list    = []
