51473a350c3c95dd26bf53c0ed421c46dadcff62
masi-sh <37276493+masi-sh@users.noreply.github.com>
2018-10-25 17:55:57 -0400

    Update Flp.py

--- Flp.py
+++ Flp.py
@@ -44,150 +44,55 @@ Queried_ID =Orgn_Ctx.iloc[Sal_outliers.argmin()][1]
 
 print '\n\n Outlier\'s ID in the original context is: ', Queried_ID
 
-#Orgn_Ctx_bar = df2.loc[(df2['Job Title'].isin([FirAtt_lst[0],FirAtt_lst[1],FirAtt_lst[2],FirAtt_lst[3],FirAtt_lst[4]])==False) & \
-#		       (df2['Employer'].isin([SecAtt_lst[0],SecAtt_lst[1], SecAtt_lst[2], SecAtt_lst[3], SecAtt_lst[4], SecAtt_lst[5]])==False) & \
-#		       (df2['Calendar Year'].isin([ThrAtt_lst[0],ThrAtt_lst[1], ThrAtt_lst[2], ThrAtt_lst[3], ThrAtt_lst[4]])==False)]
-
 Flp_Ctx = pd.DataFrame()
-############### Keeping attribute values in the original context, p =pr(1-->1)  ###############
+        ############### Keeping attribute values in the original context, p =pr(1-->1)  ###############
 Flp_p        = 0.7
-############### Adding attribute values not in the original context, q =pr(0-->1) ###############
+         ############### Adding attribute values not in the original context, q =pr(0-->1) ###############
 Flp_q        = 0.4
-FirAtt_Flp   = []
-SecAtt_Flp   = []
-ThrAtt_Flp   = []
-Flp_Sal_list = []
-###################################        Flip the context ctx_Flpr(=100) times            ###############################
-                %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%FIXED UP TO HERE%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+FirAtt_Flp   = np.zeros(len(FirAtt_lst), dtype=np.int)
+SecAtt_Flp   = np.zeros(len(SecAtt_lst), dtype=np.int)
+ThrAtt_Flp   = np.zeros(len(ThrAtt_lst), dtype=np.int)
+Flp_lst = []
 
-for Ctx_Flpr  in range (0, 100):
-	
+###################################        Flip the context ctx_Flpr(=100) times            ###############################
+
+Ctx_Flpr = 0
+while Ctx_Flpr<100:
+	##### context separator scans all elements in the attribute lists to find where to apply p or q #######
 	for Ctx_sprt in range (0, len(FirAtt_lst)):
 		if ((Ctx_sprt<5 and np.random.binomial(size=1, n=1, p= Flp_p)==1) or \
 		    (Ctx_sprt>=5 and np.random.binomial(size=1, n=1, p= Flp_q)==1)):
-			FirAtt_Flp.append(FirAtt_lst[Ctx_sprt])
+			FirAtt_Flp[Ctx_sprt]=1
 
 	for Ctx_sprt in range (0, len(SecAtt_lst)):
 		if ((Ctx_sprt<5 and np.random.binomial(size=1, n=1, p= Flp_p)==1) or \
 		    (Ctx_sprt>=5 and np.random.binomial(size=1, n=1, p= Flp_q)==1)):
-			SecAtt_Flp.append(SecAtt_lst[Ctx_sprt])
+			SecAtt_Flp[Ctx_sprt]=1
 	
 	for Ctx_sprt in range (0, len(ThrAtt_lst)):
 		if ((Ctx_sprt<5 and np.random.binomial(size=1, n=1, p= Flp_p)==1) or \
 		    (Ctx_sprt>=5 and np.random.binomial(size=1, n=1, p= Flp_q)==1)):
-			ThrAtt_Flp.append(ThrAtt_lst[Ctx_sprt])
-
-	Flp_Ctx.append(df2[(df2['Job Title'].isin(FirAtt_Flp)) & (df2['Employer'].isin(SecAtt_Flp)) & \
-			   (df2['Calendar Year'].isin(ThrAtt_Flp))])
+			ThrAtt_Flp[Ctx_sprt]=1
 	
-	for row in range(Flp_Ctx.shape[0]):
-                    Sal_list.append(Flp_Ctx.iloc[row]['Salary Paid'])
-                %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%FIXED UP TO HERE%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	Flp_Ctx= Flp_Ctx.append(df2[(df2['Job Title'].isin(FirAtt_lst[np.where(FirAtt_Flp == 1)])) & \
+				    (df2['Employer'].isin(SecAtt_lst[np.where(SecAtt_Flp == 1)])) & \
+				    (df2['Calendar Year'].isin(ThrAtt_lst[np.where(ThrAtt_Flp == 1)]))])
+	
 	if (Flp_Ctx.shape[0] >= 20):
-                #Score = np.exp(Epsilon *(pop_size** (1. / 3)))
+		for row in range(Flp_Ctx.shape[0]):
+                    Sal_list.append(Flp_Ctx.iloc[row]['Salary Paid'])
+                #Score = np.exp(Epsilon *((Flp_Ctx.shape[0])** (1. / 3)))
                 Sal_arr= np.array(Sal_list)
                 clf = LocalOutlierFactor(n_neighbors=20)
                 Sal_outliers = clf.fit_predict(Sal_arr.reshape(-1,1))
-				
-		
-		context.append([i,j,z,pop_size])
+		for outlier_finder in range(0, len(ID_list)):
+                    if ((Sal_outliers[outlier_finder]==-1) and (ID_list[outlier_finder]==Queried_ID)):  
+			Flp_lst.append([FirAtt_Flp, SecAtt_Flp, ThrAtt_Flp])
+			Ctx_Flpr+=1
+			
+                %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%FIXED UP TO HERE%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 		#outputfile.write('Context:\n'+str(context)+'\n')
 		#print '\n\nlen(ID_list) is', len(ID_list)
 		#print '\n\nSal_outliers for the context++ is', Sal_outliers, '\n\n An example of outlier here is', df2.iloc[Sal_outliers.argmin()][1]
-                for outlier_finder in range(0, len(ID_list)):
-                    if ((Sal_outliers[outlier_finder]==-1) and (ID_list[outlier_finder]==Queried_ID)):                      
-				
-				
-				Sub_pop.append([i,j,z,pop_size, Score, Sub_pop_count])
-                        	Sub_pop_count += 1
-		Sub_pop_sorted = sorted(Sub_pop,key=lambda Sub_pop: Sub_pop[3])
+               
 		
-		#print '\n\nSubpopulations are[Att1_index, Att2_index, Population_size, Score, ID]\n\n', Sub_pop	
-		print '\n\nSubpopulations sorted based on the population size are[Att1_index, Att2_index, Att2_index, Population_size, Score, ID]\n\n', \
-                Sub_pop_sorted
-		#print '\n\n str(output)', str(output)
-
-
-
-
-################# Exploring Contexts larger than the original to find the maximal #################
-FirAtt_Sprset = sum(map(lambda r: list(combinations(FirAtt_lst[5:], r)), range(1, len(FirAtt_lst[5:])+1)), [])
-SecAtt_Sprset = sum(map(lambda r: list(combinations(SecAtt_lst[6:], r)), range(1, len(SecAtt_lst[6:])+1)), [])
-ThrAtt_Sprset = sum(map(lambda r: list(combinations(ThrAtt_lst[5:], r)), range(1, len(ThrAtt_lst[5:])+1)), [])
-
-Sub_pop        =  []
-Sub_pop_count  =  0
-Epsilon        =  0.1  ### Privacy Parameter
-output         =  []
-context        =  []
-
-t0 = time.time()
-#outputfile = open(outputname,'w')
-Maxfile    = open(Maxfilename, 'a')
-
-############################## Finds Maximal ##########################################
-for i in range (0, len(FirAtt_Sprset)):
- for j in range(int(sys.argv[1]), (int(sys.argv[1])+1)):
-   for z in range(0, len(ThrAtt_Sprset)):
-            Sal_list   = []
-            ID_list    = []
-            pop_size   = 0
-            Score      = 1
-            #csvfile.seek(0)
-            print '\n\n The ', i, 'th element in the first attribute\'s superset'
-            print '\n\n The ', j, 'th element in the Second attribute\'s superset'
-            print '\n\n The ', z, 'th element in the third attribute\'s superset'
-            for row in range(df2.shape[0]):
-		# print '\n\nrow is' , df2.iloc[row]
-                # FirAtt referes to 'Job Title', which is array cell #5, 
-                # SecAtt referes to 'Employer', which is array cell #4,
-                # ThrAtt referes to 'Calendar Year', which is array cell #7,
-                # isnt union1d(FirAtt_Sprset[i], FirAtt_lst[:5])= FirAtt_Sprset[i]+ FirAtt_lst[:5]
-                if ((df2.iloc[row][5] in (np.union1d(FirAtt_Sprset[i], FirAtt_lst[:5]))) & \
-                    (df2.iloc[row][4] in  (np.union1d(SecAtt_Sprset[j], SecAtt_lst[:6]))) & \
-                     (df2.iloc[row][7] in (np.union1d(ThrAtt_Sprset[z], ThrAtt_lst[:5])))):
-                    pop_size += 1
-                    Sal_list.append(df2.iloc[row]['Salary Paid'])
-		    ID_list.append(df2.iloc[row]['Unnamed: 0'])
-
-
-##########################3                                     ##########################
-
-
-            print '\n\n Population size for orignial context plus first attribute set=', i, \
-                    'Second attribute set=', j, 'Third attribute set=', z , 'is' , pop_size
-#####################         Outlier detection in subpopulations      ########################
-            if (pop_size >= 20):
-                #Score = np.exp(Epsilon *(pop_size** (1. / 3)))
-                Sal_arr= np.array(Sal_list)
-                clf = LocalOutlierFactor(n_neighbors=20)
-                Sal_outliers = clf.fit_predict(Sal_arr.reshape(-1,1))
-		context.append([i,j,z,pop_size])
-		#outputfile.write('Context:\n'+str(context)+'\n')
-		#print '\n\nlen(ID_list) is', len(ID_list)
-		#print '\n\nSal_outliers for the context++ is', Sal_outliers, '\n\n An example of outlier here is', df2.iloc[Sal_outliers.argmin()][1]
-                for outlier_finder in range(0, len(ID_list)):
-                    if (Sal_outliers[outlier_finder]==-1): 
-			output.append(ID_list[outlier_finder]) 
-			if (ID_list[outlier_finder]==Queried_ID):                       
-				Sub_pop.append([i,j,z,pop_size, Score, Sub_pop_count])
-                        	Sub_pop_count += 1
-		Sub_pop_sorted = sorted(Sub_pop,key=lambda Sub_pop: Sub_pop[3])
-		
-		#print '\n\nSubpopulations are[Att1_index, Att2_index, Population_size, Score, ID]\n\n', Sub_pop	
-		print '\n\nSubpopulations sorted based on the population size are[Att1_index, Att2_index, Att2_index, Population_size, Score, ID]\n\n', \
-                Sub_pop_sorted
-		#print '\n\n str(output)', str(output)
-#		outputfile.write('ID_list:\n'+str(output)+'\n')
-
-#outputfile.close()
-if Sub_pop_sorted:
-	fcntl.flock(Maxfile, fcntl.LOCK_EX)
-	Maxfile.write(str(Sub_pop_sorted[Sub_pop_count-1])+'\n')
-        fcntl.flock(Maxfile, fcntl.LOCK_UN)	
-	print 'Max_population is', Sub_pop_sorted[Sub_pop_count-1]   
-
-Maxfile.close()
-
-t1 = time.time()
-print '\n\nThe required time for running the program is:',  t1-t0
