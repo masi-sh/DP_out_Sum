3e60962ca3e755846fcb7d51a65afd9f554b90d7
masi-sh <37276493+masi-sh@users.noreply.github.com>
2019-01-13 21:12:35 -0500

    Update BFS.py

--- BFS.py
+++ BFS.py
@@ -65,20 +65,17 @@ Sal_outliers = clf.fit_predict(Orgn_Ctx['Salary Paid'].values.reshape(-1,1))
 Queried_ID =Orgn_Ctx.iloc[Sal_outliers.argmin()][1]
 print '\n\n Outlier\'s ID in the original context is: ', Queried_ID
 
-        ############### Keeping attribute values in the original context, p =pr(1-->1)  ###############
-Flp_p        = 0.7
-       ############### Adding attribute values not in the original context, q =pr(0-->1) ###############
-Flp_q        = 0.4
-Flp_lst      = []
+
   ##############################        Making Queue of samples and initiating it       #################################
 BFS_Vec      = np.zeros(len(FirAtt_Vec)+len(SecAtt_Vec)+len(ThrAtt_Vec))
 np.concatenate((FirAtt_Vec, SecAtt_Vec, ThrAtt_Vec), axis=0, out=BFS_Vec)
         ################################# Initiating queue with Org_ctx informaiton  ########################
-Queue	     = [[0, np.exp(Epsilon *(np.log(Org_Ctx.shape[0]))), Orgn_Ctx.shape[0], BFS_Vec]]
+Queue	     = [[0, np.exp(Epsilon *(np.log(Orgn_Ctx.shape[0]))), Orgn_Ctx.shape[0], BFS_Vec]]
 ###################################        Flip the context ctx_Flpr(=100) times            ###############################
 Epsilon = 0.1
 Ctx_Flpr = 0
 t0 = time.time()
+BFS_Flp = np.zeros(len(BFS_Vec)) 
 
 while Ctx_Flpr<99:  
 	
@@ -102,53 +99,15 @@ while Ctx_Flpr<99:
                 Sal_outliers = clf.fit_predict(Sal_arr.reshape(-1,1))
 		for outlier_finder in range(0, len(ID_list)):
                     if ((Sal_outliers[outlier_finder]==-1) and (ID_list[outlier_finder]==Queried_ID)): 
-			Queue.append([Ctx_Flpr+1, Score, BFS_Ctx.shape[0], BFS_Flp)
+			Queue.append([Ctx_Flpr+1, Score, BFS_Ctx.shape[0], BFS_Flp])
 			print '\n Ctx_Flpr is = ', Ctx_Flpr, '\n The private context candidates are: \n', Queue
 	###################################       Sampling form the Queue ###############################
-			Q_smp  = len(Queue)
 			elements = [elem[0] for elem in Queue]	
 			probabilities = [prob[1] for prob in Queue]/(sum ([prob[1] for prob in Queue]))
-			ExpRes = np.random.choice(elements, Q_smp, p = probabilities)
-			BFS_Vec[:]  = Queue[[ExpRes][3][:]]
+			ExpRes = np.random.choice(elements, 1, p = probabilities)
+			BFS_Vec[:]  = Queue[ExpRes[0]][3][:]
 			Ctx_Flpr+=1
-			print 'The candidate picked form the Q is ', ExpRes, 'th, with context ', Queue[[ExpRes][3]],\
-				      ' and has ', Queue[[ExpRes][2]], 'population'
+			print 'The candidate picked form the Q is ', ExpRes[0], 'th, with context ', Queue[ExpRes[0]][3][:],\
+				      ' and has ', Queue[ExpRes[0]][2], 'population'
 			
-				      
-				      
-				      
-       ###################################      Sampling form Exp Mech Result      #################################
-num_smp  = 100
-elements = [elem[0] for elem in Flp_lst]	
-probabilities = [prob[1] for prob in Flp_lst]/(sum ([prob[1] for prob in Flp_lst]))
-ExpRes = np.random.choice(elements, num_smp, p = probabilities)
-print '\n\nThe number of candidates in Exponential mechanism range is:'           , len(Flp_lst)
-print '\n\nIDs sampled from Exponential mechanism output are\n\n',  ExpRes
-
-	#################################    Population size in the samples     #####################################
-Flp_Ctx_sizes =[]  			
-for ids in ExpRes:
-	Flp_Ctx_sizes.append(Flp_lst[ids][2])
-print '\n\nThe population size in the perturbed candidates are: \n\n', Flp_Ctx_sizes
-
-t1 = time.time()
-print '\n\nThe required time for running the program is:', t1-t0
-
-	#	%%%%%%%%%%%%%%%%%%%%%%%%%%%%%          FIXED UP TO HERE           %%%%%%%%5%%%%%%%%%%%%%%
-
-	########################    Sample distance from outlier(in the number of attribute values)    ##############
-print '\n\noutlier_index is: ', Queried_ID
-#Smpl_out_dist =  [(len(Flp_lst[ids][3] - FirAtt_lst) + (len(Flp_lst[ids][4] - SecAtt_lst)\
-#		+ (len(Flp_lst[ids][5] - ThrAtt_lst) for ids in ExpRes] 
-
-#print '\n\nThe distance(in the number of attribute values) between perturbed candidates and the outlier is: \n\n', \
-#       Smpl_out_dist
-
-#t1 = time.time()
-#print '\n\nThe required time for running the program is:',  t1-t0
-
-plt.figure(1)
-pd.Series(Flp_Ctx_sizes).value_counts().plot('bar')
-#plt.figure(2)
-#pd.Series(Smpl_out_dist).value_counts().plot('bar')
-plt.show()
+print '\n The final Queue is \n', Queue      
