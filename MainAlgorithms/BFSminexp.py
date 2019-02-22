5484c90b8d62fcbbfa6dafd1bc0a9e1b922a98dd
masi-sh <37276493+masi-sh@users.noreply.github.com>
2019-02-22 12:41:00 -0500

    Update BFSminexp.py

--- MainAlgorithms/BFSminexp.py
+++ MainAlgorithms/BFSminexp.py
@@ -58,7 +58,7 @@ def maxctx(Ref_file, Queried_ID):
 def writefinal(Data_to_write, randomness, runtime, ID):	
 	ff = open(Store_file,'a+')
 	fcntl.flock(ff, fcntl.LOCK_EX)
-	np.savetxt(ff, column_stack(Data_to_write), fmt=('%5i'), header = randomness+ ' Generates outlier , ' + ID + ', BFSexp alg. takes' + runtime)
+	np.savetxt(ff, np.column_stack(Data_to_write), fmt=('%5i'), header = randomness+ ' Generates outlier , ' + ID + ', BFSexp alg. takes' + runtime)
 	fcntl.flock(ff, fcntl.LOCK_UN)
 	ff.close()
 	return;
