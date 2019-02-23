fa840d10e0f695c34a7c84f45eefde6bd852db62
masi-sh <37276493+masi-sh@users.noreply.github.com>
2019-02-22 22:05:41 -0500

    Update BFSexp.py

--- MainAlgorithms/BFSexp.py
+++ MainAlgorithms/BFSexp.py
@@ -165,11 +165,14 @@ for dpt in range (Datapoints):
 		elements = [elem[0] for elem in Queue]	
 		probabilities = [prob[1] for prob in Queue]/(sum ([prob[1] for prob in Queue]))
 		ExpRes = np.random.choice(elements, 1, p = probabilities)
-		BFS_Vec[:]  = Queue[ExpRes[0]][3][:]
-		print 'The candidate picked form the Q is ', ExpRes[0], 'th, with context ', Queue[ExpRes[0]][3][:],\
-		' and has ', Queue[ExpRes[0]][2], 'population'
-	
-		Data_to_write.append(Queue[ExpRes[0]][1]) 
+		for child in range(0, len(Queue)):
+      	  		if Queue[child][0] == ExpRes[0]:
+            			QQ_indx = child        		
+		BFS_Vec[:]  = Queue[QQ_indx][3][:]
+		print 'The candidate picked form the Q is ', ExpRes[0], 'th, with context ', Queue[QQ_indx][3][:],\
+		' and has ', Queue[QQ_indx][2], 'population'
+		
+		Data_to_write.append(Queue[QQ_indx][1]) 
 	
 	###################################       Writing final data ###############################
 
