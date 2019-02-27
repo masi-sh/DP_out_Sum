894b548e8b5a39156716009dd65fbabb777ba0b4
masi-sh <37276493+masi-sh@users.noreply.github.com>
2019-02-27 11:44:19 -0500

    Update Queryinit.py

--- Queryinit.py
+++ Queryinit.py
@@ -1,21 +1,8 @@
 import sys
 import pandas as pd
 import numpy as np
-#import cufflinks as cf
-#cf.go_offline()
-#df = pd.read_csv("~/DP_out_Sum/dataset/combined.csv")
-#from itertools import combinations
-#from sklearn.neighbors import LocalOutlierFactor
-from collections import Counter
-import time
-import fcntl
-import random
 
-Titles = [['', 'Outlier', 'Ctx', 'Max']]
-for i in range(NumofQueries):
-  Titles.append([i, 0, str(np.zeros()), 0])
-   
-
-Queries = pd.DataFrame(data=data[1:,1:], index=data[1:,0], columns=data[0,1:])
-
-Queries.to_csv(Store_file)
+Query_file = '~/DP_out_Sum/MainAlgorithms/Queries.csv'
+Titles = np.array([['index', 'Outlier', 'Ctx', 'Max']])
+Queries = pd.DataFrame(data=data[1:,1:], columns=data[0,1:])
+Queries.to_csv(Query_file)
