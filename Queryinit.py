b9aed32c733a17f5211e11827e4e8cf9ee8117e3
masi-sh <37276493+masi-sh@users.noreply.github.com>
2019-02-27 11:53:46 -0500

    Update Queryinit.py

--- Queryinit.py
+++ Queryinit.py
@@ -3,6 +3,6 @@ import pandas as pd
 import numpy as np
 
 Query_file = '~/DP_out_Sum/MainAlgorithms/Queries.csv'
-Titles = np.array([['index', 'Outlier', 'Ctx', 'Max']])
+Titles = np.array([['index', 'Outlier', 'Max','Ctx']])
 Queries = pd.DataFrame(data=data[1:,1:], columns=data[0,1:])
 Queries.to_csv(Query_file)
