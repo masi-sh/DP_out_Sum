eacb10f2c2b7cc5e0e2777875a980e1ed7edbbc8
masi-sh <37276493+masi-sh@users.noreply.github.com>
2019-03-17 14:25:16 -0400

    Update BFSminexp.py

--- MainAlgorithms/BFSminexp.py
+++ MainAlgorithms/BFSminexp.py
@@ -20,6 +20,7 @@ import random
 import csv
 
 Query_num = int(sys.argv[1])
+random.seed(Query_num)
 # This file is filtered, no extra filtering required
 df2 = pd.read_csv("~/DP_out_Sum/dataset/FilteredData.csv")
 Query_file = '/home/sm2shafi/DP_out_Sum/MainAlgorithms/Queries.csv'
