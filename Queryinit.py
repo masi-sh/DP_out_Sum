7b7994eeda3e544815082957b02266ee010f3d8f
masi-sh <37276493+masi-sh@users.noreply.github.com>
2019-03-17 22:43:13 -0400

    Update Queryinit.py

--- Queryinit.py
+++ Queryinit.py
@@ -2,7 +2,7 @@ import sys
 import pandas as pd
 import numpy as np
 
-Query_file = '~/DP_out_Sum/MainAlgorithms/Queries.csv'
+Query_file = '~/DP_out_Sum/MainAlgorithms/QuerieswithMax.csv'
 Titles = np.array([['ind', 'Outlier', 'Max','Ctx', 'Max_Ctx']])
 Queries = pd.DataFrame(data=Titles[1:,1:], columns=Titles[0,1:])
 Queries.to_csv(Query_file)
