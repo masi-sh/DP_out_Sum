50d9a94db071123eebcb975f69d429fd23f8949e
masi-sh <37276493+masi-sh@users.noreply.github.com>
2019-06-03 09:11:15 -0400

    Update BFS.py

--- MainAlgorithms/BFS.py
+++ MainAlgorithms/BFS.py
@@ -106,51 +106,54 @@ def BFS_Alg(Org_Vec, Queue, Data_to_write, Epsilon, max_ctx):
     		if (Terminator>termination_threshold):
 			break
     		Addtosamples    = False
-   	print '\nSampling & Queueing...  \n',
-    	for i in  range (len(Queue[Q_indx][3])):      
-        	BFS_Flp[i]        = Queue[Q_indx][3][i]
-    	while any(np.array_equal(BFS_Flp[:],x[3][:]) for x in Queue):
-		print '\nThis is already on the queue too!'
-        	for i in  range (len(Queue[Q_indx][3])):      
-            		BFS_Flp[i]    = Queue[Q_indx][3][i]
-        	Flp_bit           = random.randint(0,(len(FirAtt_lst)+len(SecAtt_lst)+len(ThrAtt_lst)-1))
-        	BFS_Flp[Flp_bit]  = 1 - BFS_Flp[Flp_bit]
-    	BFS_Ctx  = df2.loc[df2['Job Title'].isin(FirAtt_lst[np.where(BFS_Flp[0:len(FirAtt_lst)] == 1)].tolist()) &\
-			   df2['Employer'].isin(SecAtt_lst[np.where(BFS_Flp[len(FirAtt_lst):len(FirAtt_lst)+len(SecAtt_lst)] == 1)].tolist())  &\
-			   df2['Calendar Year'].isin(ThrAtt_lst[np.where(BFS_Flp[len(FirAtt_lst)+len(SecAtt_lst):len(FirAtt_lst)+len(SecAtt_lst)+len(ThrAtt_lst)] == 1)].tolist())]
-    	Sal_list     = []
-    	ID_list      = []
-    	if (BFS_Ctx.shape[0] >= 20):
-        	for row in range(BFS_Ctx.shape[0]):
-            		Sal_list.append(BFS_Ctx.iloc[row]['Salary Paid'])
-            		ID_list.append(BFS_Ctx.iloc[row]['Unnamed: 0'])
+   		print '\nSampling & Queueing...  \n',
+    		for i in  range (len(Queue[Q_indx][3])):      
+        		BFS_Flp[i]        = Queue[Q_indx][3][i]
+    		while any(np.array_equal(BFS_Flp[:],x[3][:]) for x in Queue):
+			print '\nThis is already on the queue too!'
+        		for i in  range (len(Queue[Q_indx][3])):      
+            			BFS_Flp[i]    = Queue[Q_indx][3][i]
+        		Flp_bit           = random.randint(0,(len(FirAtt_lst)+len(SecAtt_lst)+len(ThrAtt_lst)-1))
+        		BFS_Flp[Flp_bit]  = 1 - BFS_Flp[Flp_bit]
+    		BFS_Ctx  = df2.loc[df2['Job Title'].isin(FirAtt_lst[np.where(BFS_Flp[0:len(FirAtt_lst)] == 1)].tolist()) &\
+				   df2['Employer'].isin(SecAtt_lst[np.where(BFS_Flp[len(FirAtt_lst):len(FirAtt_lst)+len(SecAtt_lst)] == 1)].tolist())  &\
+				   df2['Calendar Year'].isin(ThrAtt_lst[np.where(BFS_Flp[len(FirAtt_lst)+len(SecAtt_lst):len(FirAtt_lst)+len(SecAtt_lst)+len(ThrAtt_lst)] == 1)].tolist())]
+    		Sal_list     = []
+    		ID_list      = []
+    		if (BFS_Ctx.shape[0] >= 20):
+        		for row in range(BFS_Ctx.shape[0]):
+            			Sal_list.append(BFS_Ctx.iloc[row]['Salary Paid'])
+            			ID_list.append(BFS_Ctx.iloc[row]['Unnamed: 0'])
 
-        	Sal_arr= np.array(Sal_list)
-        	clf = LocalOutlierFactor(n_neighbors=20)
-        	Sal_outliers = clf.fit_predict(Sal_arr.reshape(-1,1))
-        	for outlier_finder in range(0, len(ID_list)):
-            		if ((Sal_outliers[outlier_finder]==-1) and (ID_list[outlier_finder]==Queried_ID)): 
-                		Score = math.exp(Epsilon *(BFS_Ctx.shape[0]))
-                		Queue.append([len(Queue), Score, BFS_Ctx.shape[0], np.zeros(len(Org_Vec))])
-				Addtosamples = True
-				Terminator   = 0
-                		for i in  range (len(Queue[len(Queue)-1][3])):      
-                    			Queue[len(Queue)-1][3][i]  = BFS_Flp[i]
+        		Sal_arr= np.array(Sal_list)
+        		clf = LocalOutlierFactor(n_neighbors=20)
+        		Sal_outliers = clf.fit_predict(Sal_arr.reshape(-1,1))
+        		for outlier_finder in range(0, len(ID_list)):
+            			if ((Sal_outliers[outlier_finder]==-1) and (ID_list[outlier_finder]==Queried_ID)): 
+                			Score = math.exp(Epsilon *(BFS_Ctx.shape[0]))
+                			Queue.append([len(Queue), Score, BFS_Ctx.shape[0], np.zeros(len(Org_Vec))])
+					Addtosamples = True
+					Terminator   = 0
+                			for i in  range (len(Queue[len(Queue)-1][3])):      
+                    				Queue[len(Queue)-1][3][i]  = BFS_Flp[i]
 
-   	 # Sampling form the Queue
-    	elements = [elem[0] for elem in Queue]
-    	probabilities =[]
-    	for prob in Queue:
-		probabilities.append(prob[1]/(sum ([prob[1] for prob in Queue])))
-    	ExpRes = np.random.choice(elements, 1, p = probabilities)
-    	for child in range(0, len(Queue)):
-        	if Queue[child][0] == ExpRes[0]:
-            		Q_indx = child
-    	if (Addtosamples):
-		Data_to_write.append((Queue[Q_indx][2])/max_ctx) 
+   		# Sampling form the Queue
+    		elements = [elem[0] for elem in Queue]
+    		probabilities =[]
+    		for prob in Queue:
+			probabilities.append(prob[1]/(sum ([prob[1] for prob in Queue])))
+    		ExpRes = np.random.choice(elements, 1, p = probabilities)
+    		for child in range(0, len(Queue)):
+        		if Queue[child][0] == ExpRes[0]:
+            			Q_indx = child
+    		if (Addtosamples):
+			Data_to_write.append((Queue[Q_indx][2])/max_ctx) 
+			print 'In BFS_Alg, Data_to_write is: ', Data_to_write
 	return;
 
 BFS_Alg(Org_Vec, Queue, Data_to_write, Epsilon, max_ctx)
+print 'Out BFS_Alg, Data_to_write is: ', Data_to_write
+
 Data_to_write = np.append(Data_to_write , np.zeros(100 - len(Data_to_write)))
 #print 'The candidate picked form the Q is ', ExpRes[0], 'th, with context ', Queue[ExpRes[0]][3][:],' and has ', Queue[ExpRes[0]][2], 'population'
 t1 = time.time()
