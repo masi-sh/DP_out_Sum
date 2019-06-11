9b54f87976886bcfaefeb4fbaf4c6c21411b177e
masi-sh <37276493+masi-sh@users.noreply.github.com>
2019-06-11 04:26:59 -0400

    Update RndQueryInit.py

--- Other/RndQueryInit.py
+++ Other/RndQueryInit.py
@@ -2,7 +2,7 @@ import sys
 import pandas as pd
 import numpy as np
 
-Query_file = '~/DP_out_Sum/dataset/RndQueries.csv'
+Query_file = '/home/sm2shafi/DP_out_Sum/dataset/RndQueries.csv'
 Titles = np.array([['ind', 'Outlier', 'Max','Ctx']])
 Queries = pd.DataFrame(data=Titles[1:,1:], columns=Titles[0,1:])
 Queries.to_csv(Query_file)
