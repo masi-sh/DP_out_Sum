606f91788bfe7e277314aa270093f601697eb866
masi-sh <37276493+masi-sh@users.noreply.github.com>
2019-06-27 23:03:32 -0400

    Update GQueryInit.py

--- Grubbs/GQueryInit.py
+++ Grubbs/GQueryInit.py
@@ -2,7 +2,7 @@ import sys
 import pandas as pd
 import numpy as np
 
-Query_file = '~/sm2shafi/DP_out_Sum/Grubbs/TGQueries.csv'
+Query_file = '~/DP_out_Sum/Grubbs/TGQueries.csv'
 Titles = np.array([['ind', 'Outlier', 'Max','Ctx', 'Max_Ctx']])
 Queries = pd.DataFrame(data=Titles[1:,1:], columns=Titles[0,1:])
 Queries.to_csv(Query_file)
