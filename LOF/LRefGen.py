31eae82f9340c65adaa5bfaa50de29598ebbbe7e
masi-sh <37276493+masi-sh@users.noreply.github.com>
2019-06-28 08:14:44 -0400

    Update LRefGen.py

--- LOF/LRefGen.py
+++ LOF/LRefGen.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 from itertools import combinations
 from collections import Counter
+from sklearn.neighbors import LocalOutlierFactor
 import time
 import fcntl
 import random
