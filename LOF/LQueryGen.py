895a40a90004e46b1dfebc0b53a138c242eb81da
masi-sh <37276493+masi-sh@users.noreply.github.com>
2019-06-28 07:46:31 -0400

    Update LQueryGen.py

--- LOF/LQueryGen.py
+++ LOF/LQueryGen.py
@@ -1,8 +1,6 @@
 import sys
 import pandas as pd
 import numpy as np
-import cufflinks as cf
-cf.go_offline()
 from itertools import combinations
 from sklearn.neighbors import LocalOutlierFactor
 from collections import Counter
@@ -10,81 +8,61 @@ import time
 import fcntl
 import random
 import csv
+import math
 
 random.seed(50*int(sys.argv[1]))
 query_num = int(sys.argv[1])
-Query_file = '/home/sm2shafi/DP_out_Sum/LOF/TLQueries.csv'
-Queries = pd.read_csv(Query_file)
 df2 = pd.read_csv("~/DP_out_Sum/Grubbs/ToyData.csv")
-Ref_file = '/home/sm2shafi/LOF/LOFRef.txt'
+Query_file = '/home/sm2shafi/DP_out_Sum/Grubbs/TGQueries.csv'
+Queries = pd.read_csv(Query_file)
+Ref_file = '/home/sm2shafi/DP_out_Sum/Grubbs/GrubbsRef.txt'
 
 def maxctx(Ref_file, Queried_ID):
-	max         = 0
-	out_size    = 0
-	line_num    = 0
-	size        = 0
+	max  = 0
+	size = 0
 	outlier_ctr = 0
-	Ctx_line    = 0
-	Ctx_Max     = ''
 	with open(Ref_file,'rt') as f:
         	for num, line in enumerate(f, 1):
-                	if line.split(' ')[0].strip()=="Matching":
-                          	Ctx_line = num
-                        	size = int((line.split(' '))[5].strip(':\n'))
-			elif line.strip().startswith("ID"):
-			        if line.split(' ')[3].strip('#')==str(Queried_ID):
-					out_size = size
-					outlier_ctr += 1
-					Valid_line = Ctx_line
-                	if (max < out_size):
-			        max = out_size
-				line_num = Valid_line 
-	#print "max so far is :", max, "in line number ", line_num
+			ctx = line[1:-2].split(',')
+                	size = int(ctx[3])
+			for outliers in range(len(ctx)):
+				if int(ctx[outliers])==ID:
+   	        			outlier_ctr += 1
+					if (max < size):
+						max = size
+					break				
 	f.close()
-	# Ctx_Max not necessery now, I just put 0 for it
-	Ctx_Max = 0
-	#with open(Ref_file,'rt') as ff:
-	#	print "\nMax context is wiht size", max ,"is:\n"
-	#	for i, x in enumerate(ff):
-	#		if i in range (line_num+1, line_num+4):
-	#			print x
-	#			Ctx_Max = Ctx_Max + x
-	#ff.close()
-  	return max, outlier_ctr, Ctx_Max;
+	return max, outlier_ctr;
 
 FirAtt_lst = df2['Job Title'].unique()
 SecAtt_lst = df2['Employer'].unique()
 ThrAtt_lst = df2['Calendar Year'].unique()
-FirAtt_Vec   = np.zeros(len(FirAtt_lst), dtype=np.int)
-SecAtt_Vec   = np.zeros(len(SecAtt_lst), dtype=np.int)
-ThrAtt_Vec   = np.zeros(len(ThrAtt_lst), dtype=np.int)
-###################################     Forming a context   #######################################
-Sal_outliers = np.array([1])
-while(Sal_outliers[Sal_outliers.argmin()]==1):
-	FirAtt_Vec[0:len(FirAtt_Vec)] = np.random.randint(2, size=len(FirAtt_Vec))
-	SecAtt_Vec[0:len(SecAtt_Vec)] = np.random.randint(2, size=len(SecAtt_Vec))
-	ThrAtt_Vec[0:len(ThrAtt_Vec)] = np.random.randint(2, size=len(ThrAtt_Vec))
 
-	Orgn_Ctx = df2.loc[df2['Job Title'].isin(FirAtt_lst[np.where(FirAtt_Vec== 1)].tolist()) & \
-			   df2['Employer'].isin(SecAtt_lst[np.where(SecAtt_Vec== 1)].tolist()) & \
-			   df2['Calendar Year'].isin(ThrAtt_lst[np.where(ThrAtt_Vec== 1)].tolist())]
-#######################     Finding an outlier in the selected context      #######################
-	if (Orgn_Ctx.shape[0] > 20):
+# Supersets for each attribute
+FirAtt_Sprset = sum(map(lambda r: list(combinations(FirAtt_lst[0:], r)), range(1, len(FirAtt_lst[0:])+1)), [])
+SecAtt_Sprset = sum(map(lambda r: list(combinations(SecAtt_lst[0:], r)), range(1, len(SecAtt_lst[0:])+1)), [])
+ThrAtt_Sprset = sum(map(lambda r: list(combinations(ThrAtt_lst[0:], r)), range(1, len(ThrAtt_lst[0:])+1)), [])
+
+outliers = []
+while (Sal_outliers[Sal_outliers.argmin()]==1):
+	i = np.random.randint(len(FirAtt_Sprset)-1)
+	j = np.random.randint(len(SecAtt_Sprset)-1)
+	z = np.random.randint(len(ThrAtt_Sprset)-1)
+	Ctx  = df2.loc[df2['Job Title'].isin(FirAtt_Sprset[i]) & df2['Employer'].isin(SecAtt_Sprset[j]) &\
+		       df2['Calendar Year'].isin(ThrAtt_Sprset[z])]
+	#outliers.append([i, j, z, Ctx.shape[0]])
+        if (Ctx.shape[0]>20):
 		clf = LocalOutlierFactor(n_neighbors=20)
 		Sal_outliers = clf.fit_predict(Orgn_Ctx['Salary Paid'].values.reshape(-1,1))
+		
 Queried_ID =Orgn_Ctx.iloc[Sal_outliers.argmin()][1]
-#print '\n\n Outlier\'s ID in the original context is: ', Queried_ID
-max_ctx, count, Ctx_Max = maxctx(Ref_file, Queried_ID)
-
-  ###########       Making Queue of samples and initiating it, with Org_Vec   ############################
-Org_Vec      = np.zeros(len(FirAtt_Vec)+len(SecAtt_Vec)+len(ThrAtt_Vec))
-np.concatenate((FirAtt_Vec, SecAtt_Vec, ThrAtt_Vec), axis=0, out=Org_Vec)
-#print '\n Org_Vec is: ' , Org_Vec
+max_ctx, count = maxctx(Ref_file, Queried_ID)
 
 if (max_ctx !=0 and count>500):
+	print 'count>500!'
 	with open(Query_file, 'ab') as csvfile:
-        	writer = csv.writer(csvfile)
-        	fcntl.flock(csvfile, fcntl.LOCK_EX)
-        	writer.writerow([query_num, Queried_ID, max_ctx, str(Org_Vec), Ctx_Max])
-        	fcntl.flock(csvfile, fcntl.LOCK_UN)
+       		writer = csv.writer(csvfile)
+       		fcntl.flock(csvfile, fcntl.LOCK_EX)
+       		writer.writerow([query_num, ID, max_ctx, str(i+1000*j+1000000*z)])
+       		fcntl.flock(csvfile, fcntl.LOCK_UN)
 	csvfile.close()
