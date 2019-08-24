9f66dfb262218ff2d706950f15411bcdfa3acd11
masi-sh <37276493+masi-sh@users.noreply.github.com>
2019-08-24 11:26:10 -0400

    Update ExpMech.py

--- MainAlgorithms/ExpMech.py
+++ MainAlgorithms/ExpMech.py
@@ -23,3 +23,44 @@ Query_file = '/home/sm2shafi/DP_out_Sum/MainAlgorithms/Queries.csv'
 Queries = pd.read_csv(Query_file, 'rt', delimiter=',' , engine = 'python')
 Store_file = 'Exp.dat'
 
+Queried_ID = Queries.iloc[Query_num]['Outlier']
+print '\n\n Outlier\'s ID in the original context is: ', Queried_ID
+# finding maximal context's size for queried_ID
+max_ctx = Queries.iloc[Query_num]['Max']
+print '\nmaximal context has the population :\n', max_ctx
+
+Data_to_write = []
+t0 = time.time()
+# Exp Mech on Queried_ID      
+def Exp_Mech(Ref_file, Queried_ID, max_ctx):
+	out_size    = 0
+	size        = 0
+	with open(Ref_file,'rt') as f:
+      Exp_Can = []
+      for num, line in enumerate(f, 1):
+          if line.split(' ')[0].strip()=="Matching":
+              size = int((line.split(' '))[5].strip(':\n'))
+          elif line.strip().startswith("ID"):
+				      if line.split(' ')[3].strip('#')==str(Queried_ID):
+					        out_size = size
+  f.close()
+  Exp_Can.append(out_size)
+	elements = [elem for elem in range(len(Exp_Can))]
+	probabilities =[]
+	for prob in Exp_Can:
+	   probabilities.append(prob/(sum(Exp_Can)))
+	Res = np.random.choice(elements, 1, p = probabilities)
+	Exp = Exp_Can[Res[0]]/max_ctx
+	return Exp;
+
+t1 = time.time()
+runtime = str(int((t1-t0) / 3600)) + ' hours and ' + str(int(((t1-t0) % 3600)/60)) + \
+	' minutes and ' + str(((t1-t0) % 3600)%60) + ' seconds\n'
+	Data_to_write.append(Exp_Mech(Ref_file, Queried_ID, max_ctx))
+
+  ff = open(Store_file,'a+')
+	fcntl.flock(ff, fcntl.LOCK_EX)
+	np.savetxt(ff, np.column_stack(Data_to_write), fmt=('%7.5f'), header = randomness+ ' Generates outlier , ' + ID + ', \
+	Exp alg. takes' + runtime)
+	fcntl.flock(ff, fcntl.LOCK_UN)
+	ff.close()
