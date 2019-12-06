e772fb43e44947d53f8b4d1ffe16a1f83965711f
masi-sh <37276493+masi-sh@users.noreply.github.com>
2019-12-06 15:44:59 -0500

    Update QueryInit.py

--- Murder/LOF/QueryInit.py
+++ Murder/LOF/QueryInit.py
@@ -2,7 +2,7 @@ import sys
 import pandas as pd
 import numpy as np
 
-Query_file = '~/DP_out_Sum/MainAlgorithms/Queries500.csv'
+Query_file = '~/DP_out_Sum/Murder/LOF/MLQueries.csv'
 Titles = np.array([['ind', 'Outlier', 'Max','Ctx', 'Max_Ctx']])
 Queries = pd.DataFrame(data=Titles[1:,1:], columns=Titles[0,1:])
 Queries.to_csv(Query_file)
