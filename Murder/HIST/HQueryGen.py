b4d912943d59254b7420fd3de91ab7332b4ef98f
masi-sh <37276493+masi-sh@users.noreply.github.com>
2019-12-16 16:16:10 -0500

    Update HQueryGen.py

--- Murder/HIST/HQueryGen.py
+++ Murder/HIST/HQueryGen.py
@@ -71,8 +71,8 @@ while True:
 		break
 		
 max_ctx, count = maxctx(Ref_file, ID)
-if (max_ctx !=0 and count>150):
-	print 'count>150! ', count
+if (max_ctx !=0 and count>120):
+	print 'count>120! ', count
 	with open(Query_file, 'ab') as csvfile:
        		writer = csv.writer(csvfile)
        		fcntl.flock(csvfile, fcntl.LOCK_EX)
