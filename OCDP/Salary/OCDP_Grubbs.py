41de82806d5b2d26d7b28184972d06b817b34dac
masi-sh <37276493+masi-sh@users.noreply.github.com>
2020-01-11 20:51:35 -0500

    Update OCDP_Grubbs.py

--- OCDP/Salary/OCDP_Grubbs.py
+++ OCDP/Salary/OCDP_Grubbs.py
@@ -74,7 +74,7 @@ def neighbor_ctx(df, ndf, Queried_ID):
                                                                 #n_ctx.append(int(i)+ 1000*int(j) + 1000000*int(z))
                                                                 n_ctx.append([i,j,z])
         n_ctx = sorted(n_ctx)
-        print 'size of o_ctx is:', len(o_ctx)
+        print 'size of n_ctx is:', len(n_ctx)
 	return n_ctx;   
         
 def neighbors_compare(o_ctx , n_ctx, match_num):
