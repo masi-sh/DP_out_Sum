2ffb5cb61f1fe3b33f884de38886896010480b24
masi-sh <37276493+masi-sh@users.noreply.github.com>
2020-01-13 19:57:19 -0500

    Update OCDP_Grubbs.py

--- OCDP/Murder/OCDP_Grubbs.py
+++ OCDP/Murder/OCDP_Grubbs.py
@@ -22,22 +22,34 @@ Query_file = '/home/sm2shafi/DP_out_Sum/Murder/Grubbs/MGQueries_28.csv'
 Queries = pd.read_csv(Query_file)
 # Check if the next line works
 Queried_ID = int(Queries.iloc[query_num,1])
-OutFile = 'M_OCDPMatch_G.txt'
+OutFile  = 'M_OCDPMatch_G.txt'
+MatchFile = 'Sal_Match_G.txt'
+NMatchFile = 'Sal_NMatch_G.txt'
 NumofNeighbors = 50
 DropThr = 1
 
-def org_ctx(Ref_file, Queried_ID):
+def org_ctx(df, Ref_file, Queried_ID):
+	FirAtt_lst = df['Weapon'].unique()
+  	SecAtt_lst = df['State'].unique()
+  	ThrAtt_lst = df['AgencyType'].unique()
+  	# Supersets for each attribute
+  	FirAtt_Sprset = sum(map(lambda r: list(combinations(FirAtt_lst[0:], r)), range(1, len(FirAtt_lst[0:])+1)), [])
+  	SecAtt_Sprset = sum(map(lambda r: list(combinations(SecAtt_lst[0:], r)), range(1, len(SecAtt_lst[0:])+1)), [])
+  	ThrAtt_Sprset = sum(map(lambda r: list(combinations(ThrAtt_lst[0:], r)), range(1, len(ThrAtt_lst[0:])+1)), [])
 	with open(Ref_file,'rt') as f:
 		o_ctx = []
+		o_ctx_shape = []
     		for num, line in enumerate(f, 1):
       			ctx = line[1:-2].split(',')
-			# Double check, chnaged for outliers in range(len(ctx)) to for outliers in range(4, len(ctx))
       			for outliers in range(4, len(ctx)):
 				if int(ctx[outliers])==Queried_ID:
-          				# Double check if this holds: [ctx[0],ctx[1],ctx[2]] = [i, j, z]
-          				o_ctx.append([int(ctx[0]), int(ctx[1]), int(ctx[2])])	
+          				o_ctx.append([int(ctx[0]), int(ctx[1]), int(ctx[2])])
+					o_ctx_db  = ndf.loc[ndf['Weapon'].isin(FirAtt_Sprset[int(ctx[0]]) &\
+											     ndf['State'].isin(SecAtt_Sprset[int(ctx[1]]) & ndf['AgencyType'].isin(ThrAtt_Sprset[int(ctx[2]])]
+					o_ctx_shape.append(o_ctx_db.shape[0])				
 	f.close()
-	o_ctx = sorted(o_ctx)
+	#o_ctx = sorted(o_ctx)
+	print 'size of o_ctx is:', len(o_ctx)
 	return o_ctx;
         
 def neighbor_ctx(df, ndf, Queried_ID):
@@ -50,11 +62,11 @@ def neighbor_ctx(df, ndf, Queried_ID):
   	ThrAtt_Sprset = sum(map(lambda r: list(combinations(ThrAtt_lst[0:], r)), range(1, len(ThrAtt_lst[0:])+1)), [])
   	ctx_count = 0
   	n_ctx = []
+	n_ctx_shape = []
   	for i in range (0, len(FirAtt_Sprset)):
     		for j in range (0, len(SecAtt_Sprset)):
       			for z in range(0, len(ThrAtt_Sprset)):
                 		ctx_count+=1
-                		print 'count is:', ctx_count #, ' The percentage done: %', ctx_count//(2**14) 
                 		Ctx  = ndf.loc[ndf['Weapon'].isin(FirAtt_Sprset[i]) & ndf['State'].isin(SecAtt_Sprset[j]) &\
 					       ndf['AgencyType'].isin(ThrAtt_Sprset[z])]
 				if (Ctx.shape[0] > 20):
@@ -65,14 +77,23 @@ def neighbor_ctx(df, ndf, Queried_ID):
 						for GOutlier in grubbs_result:
                                 			if (IDs.values[GOutlier]==Queried_ID):
 								n_ctx.append([i,j,z])
-  	n_ctx = sorted(n_ctx)
+								n_ctx_shape.append(Ctx.shape[0])
+  	#n_ctx = sorted(n_ctx)
         print 'size of n_ctx is:', len(n_ctx)
 	return n_ctx;   
         
 def neighbors_compare(o_ctx , n_ctx, match_num):
-  	# Caution: the following considers the permutation as inequality, double check if n_ctx has the same order as o_ctx or sort first
-  	if (np.array_equal(o_ctx,n_ctx)):
+  	if (np.array_equal(sorted(o_ctx),sorted(n_ctx))):
     		match_num+=1
+		writefinal(MatchFile, O_ctx[:])
+		writefinal(MatchFile, o_ctx_shape[:])
+		writefinal(MatchFile, n_ctx[:])
+		writefinal(MatchFile, n_ctx_shape[:])
+	else:	
+		writefinal(NMatchFile, O_ctx[:])
+		writefinal(NMatchFile, o_ctx_shape[:])
+		writefinal(NMatchFile, n_ctx[:])
+		writefinal(NMatchFile, n_ctx_shape[:])
   	return match_num;   
 
 def writefinal(OutFile, match_num):
@@ -84,7 +105,7 @@ def writefinal(OutFile, match_num):
         return;
 
 t0 = time.time()  
-o_ctx = org_ctx(Ref_file, Queried_ID)
+o_ctx = org_ctx(df, Ref_file, Queried_ID)
 match_num = 0
 for neighbor in range (0, NumofNeighbors):
   	ndf = pd.DataFrame()
@@ -97,4 +118,4 @@ for neighbor in range (0, NumofNeighbors):
 writefinal(OutFile, match_num)
 t1 = time.time()
 runtime = str(int((t1-t0) / 3600)) + ' hours and ' + str(int(((t1-t0) % 3600)/60)) +' minutes and ' + str(((t1-t0) % 3600)%60) + ' seconds\n'
-print '\n OCDPMatch runtime,for LOF and ' , NumofNeighbors, ' neighbors is: \n', runtime
+print '\n OCDPMatch runtime,for Grubbs and ' , NumofNeighbors, ' neighbors is: \n', runtime
